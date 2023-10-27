0618638e0979ed41ee40b4a41bf09750ac128c52
Ralph Walther <rw@entimo.de>
2023-10-27 14:20:05 +0200

    ups

---
+++ src/main/java/com/entimo/worklogsync/utile/ProjectMapper.java
@@ -0,0 +1,12 @@
+package com.entimo.worklogsync.utile;
+
+import com.entimo.worklogsync.postgresql.data.JiraProject;
+import java.util.List;
+
+public class ProjectMapper {
+ private List<JiraProject> jiraProjectList;
+
+ private enum JiraPepProjectEnum{
+
+ }
+}
